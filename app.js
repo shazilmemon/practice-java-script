eb39735158d393aada39b2d0c220806eb2052b5d
Shazil <shazilmemon6@gmail.com>
2022-12-05 12:25:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+// alert("hello world");
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c)
+
+
+// var x = 20
+// var y= 40
+// alert(y)
+
+
+// var a = prompt("enter your age","your age is:");
+// alert(a);
+
+
+// var percent = +prompt("enter your percent")
+// if(prcent>= 80 && percent<= 100){  
+// alert("A+")
+// }
+
+// else if(percent>= 70 && percent<= 80){
+
+//     alert("A")
+// }
+
+// else if(percent>= 60 && percent<= 70){ 
+
+//     alert("B")
+// }
+
+// else if(percent>= 50 && percent<= 60){ 
+
+//     alert("c")
+// }
+
+// else if(percent>= 40 && percent<= 50){ 
+
+//     alert("D")
+// }
+
+
+// else if(percent>= 33 && percent<= 40){ 
+
+//     alert("E")
+// }
+
+
+// else if(percent>= 0 && percent<= 33){ 
+
+//     alert("F")
+// }
+
+// else alert("you did not gain correct percentage")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("munjo naalo shazil ahai")
+
+
+
+var a = 20 
+var b = 40
+if(a==b){alert("munjo naamlo shazil ahai");}
+else if (a != b){
+    alert("munjo naalo shazil na ahai")
+}
+else{
+    alert("matrix tech")
+}
+
